4b8f9331d43a237bc2f6492e75a9d4e1cfd35c59
AndreRicardoSc <andrenarutto7@gmail.com>
2026-03-25 12:25:04 -0300

    FEAT: Creating UserCard props

---
+++ src/components/cards/UserCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    full_name: {
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: Number,
+        default: null
+    },
+    longitude: {
+        type: Number,
+        default: null
+    },
+})
+</script>
+
+<template>
+    <RouterLink to="/">
+        
+    </RouterLink>
+</template>
