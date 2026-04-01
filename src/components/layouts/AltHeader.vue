5f21a2ee76d11362ee727deea6d4e2ba84f0c4a7
Johann5731 <johannmborges27@gmail.com>
2026-04-01 09:26:59 -0300

    FEAT: Work in progress #12

--- src/components/layouts/AltHeader.vue
+++ src/components/layouts/AltHeader.vue
@@ -1,16 +1,17 @@
 <script setup>
 import {useRouter} from 'vue-router';
 const router = useRouter();
+const route = useRouter();
 </script>
 <template>
   <div class="flex justify-around">
-    <button @click="router.back()">
+    <button class="text-2xl border border-doggo-gray text-doggo-green rounded-xl px-1.5" @click="router.back()">
       <span class="mdi mdi-arrow-left"></span>
     </button>
     <RouterLink to="/">
         <img class="w-25 mr-3" src="/logo.png" alt="">
     </RouterLink>
-    <RouterLink to="/notifications">
+    <RouterLink v-if="route.value === '/notifications'" to="/notifications">
       <div class="text-2xl border border-doggo-gray text-doggo-green rounded-xl px-1.5 pb-0.5">
         <span class="mdi mdi-bell-outline"></span>
       </div>
