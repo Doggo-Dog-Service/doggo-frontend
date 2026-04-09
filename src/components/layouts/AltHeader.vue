ff2c91c4fc247784f0a1d46ac247300468ea7279
Johann5731 <johannmborges27@gmail.com>
2026-04-09 18:24:34 -0300

    FEAT: AltHeader construction finished #12

--- src/components/layouts/AltHeader.vue
+++ src/components/layouts/AltHeader.vue
@@ -1,18 +1,24 @@
 <script setup>
 import {useRouter} from 'vue-router';
+import {useRoute} from 'vue-router'
+
 const router = useRouter();
-const route = useRouter();
+const route = useRoute();
 </script>
 <template>
-  <div class="flex justify-around">
+  <div class="flex justify-between  p-4">
     <button class="text-2xl border border-doggo-gray text-doggo-green rounded-xl px-1.5" @click="router.back()">
       <span class="mdi mdi-arrow-left"></span>
     </button>
     <RouterLink to="/">
-        <img class="w-25 mr-3" src="/logo.png" alt="">
+        <img class="w-25 mr-3" src="/logo.png" alt="Doggo">
     </RouterLink>
-    <RouterLink v-if="route.value === '/notifications'" to="/notifications">
-      <div class="text-2xl border border-doggo-gray text-doggo-green rounded-xl px-1.5 pb-0.5">
+    <RouterLink to="/notifications">
+      <div :class="[
+        route.path === '/notificações'
+        ? 'text-2xl border border-doggo-gray text-doggo-green rounded-xl px-1.5 pb-0.5'
+        : 'text-2xl border border-doggo-gray text-doggo-green rounded-xl px-1.5 pb-0.5 invisible'
+        ]">
         <span class="mdi mdi-bell-outline"></span>
       </div>
     </RouterLink>
