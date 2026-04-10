deed74c32d1c429d1f8cd1b7914238cc789a607c
Johann5731 <johannmborges27@gmail.com>
2026-04-10 14:52:38 -0300

    FEAT: Work in progress #18

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,28 @@
 <script setup>
-import { onMounted } from 'vue';
-import AppHeader from './components/layouts/AppHeader.vue';
-import AltHeader from './components/layouts/AltHeader.vue';
-import MobileNavBar from './components/layouts/MobileNavBar.vue';
-import { useRoute } from 'vue-router';
-import { useAuthStore } from './stores/auth';
-const authStore = useAuthStore();
+import { onMounted } from 'vue'
+import AppHeader from './components/layouts/AppHeader.vue'
+import AltHeader from './components/layouts/AltHeader.vue'
+import MobileNavBar from './components/layouts/MobileNavBar.vue'
+import { useRoute } from 'vue-router'
+import { useAuthStore } from './stores/auth'
+const authStore = useAuthStore()
 
-const route = useRoute();
- 
-onMounted(async() => {
-  await authStore.fetchUser();
-});
+const route = useRoute()
+
+onMounted(async () => {
+  await authStore.fetchUser()
+})
 </script>
 
 <template>
   <header v-if="route.path === '/'">
-    <AppHeader/>
+    <AppHeader />
   </header>
   <header v-else>
-    <AltHeader/>
+    <AltHeader />
   </header>
-  <div class="pb-20">
-    <RouterView v-slot="{ Component }" class="">
-      <Transition name="fade" mode="out-in">
-        <component :is="Component"/>
-      </Transition>
-    </RouterView>
-  </div>
-  <MobileNavBar/>
+  <main class="pb-20">
+    <RouterView />
+  </main>
+  <MobileNavBar />
 </template>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 300ms ease, transform 300ms ease;
-}
-
-.fade-enter-from {
-  opacity: 0;
-  transform: translateY(10px);
-}
-
-.fade-leave-to{
-  opacity: 0;
-  transform: translateY(10px);
-}
-</style>
