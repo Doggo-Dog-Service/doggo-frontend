a42223b742bb4dc3cce983849c13411498eba42d
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-13 22:46:18 -0300

    FEAT: Adding base map to RegisterLocationView #16

--- src/views/RegisterLocationView.vue
+++ src/views/RegisterLocationView.vue
@@ -1,24 +1,37 @@
 <script setup>
 import L from 'leaflet'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 
 let map
 
-onMounted(() => {
-  map = L.map('map').setView([-26.3044, -48.8487], 13)
+onMounted(async() => {
+  
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-  }).addTo(map)
+      map = L.map('map').setView([lat, lng], 13)
 
-  L.marker([-26.3044, -48.8487])
-    .addTo(map)
-    .bindPopup('Dog Walker aqui 🐶')
-})
-
-
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors',
+      }).addTo(map)
+      
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup('Você está aqui')
+        .openPopup()
+    }, (error) => console.log(error), {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 0
+    })
+  }
+})  
+  
 </script>
 
 <template>
-  <div id="map" style="height: calc(100vh/1.2);"></div>
+  <div id="map" style="height: calc(100vh / 1.2)"></div>
 </template>
