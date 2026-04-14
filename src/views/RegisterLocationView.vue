bbeb142e6a76f5386720d8b6afd6d368d6a9fc7c
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-14 20:39:31 -0300

    FEAT: Adding reactive data in RegisterLocationView #16

--- src/views/RegisterLocationView.vue
+++ src/views/RegisterLocationView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import IconButton from '@/components/buttons/IconButton.vue'
 import L from 'leaflet'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
-let map
+const data = ref({
+  fixed_latitude: 0,
+  fixed_longitude: 0
+})
 
 const mapTile = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors',
@@ -26,6 +29,8 @@ const baseMaps = {
   DarkMode: darkTile,
 }
 
+let map 
+
 onMounted(async () => {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(
@@ -35,7 +40,7 @@ onMounted(async () => {
 
         map = L.map('map', {
           zoomControl: false,
-          layers: [mapTile],
+          layers: [mapTile]
         }).setView([lat, lng], 13)
 
         L.control.layers(baseMaps).addTo(map)
@@ -43,7 +48,8 @@ onMounted(async () => {
         L.marker([lat, lng]).addTo(map).bindPopup('Você está aqui').openPopup()
 
         map.addEventListener('click', (event) => {
-          console.log(event.latlng)
+          data.value.fixed_latitude = event.latlng.lat
+          data.value.fixed_longitude = event.latlng.lng
         })
       },
       (error) => console.log(error),
@@ -58,12 +64,15 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="w-full h-full p-4 relative overflow-hidden">
+  <div class="w-full h-full pb-4 px-4 relative overflow-hidden">
     <div id="map" class="w-full h-full rounded-xl z-0"></div>
     <IconButton
       class="absolute bottom-12 right-8"
       icon="mdi mdi-check"
       tooltip="Confirmar localização"
     />
+    <div class="absolute top-0 bg-black/40 w-60 rounded-br-xl rounded-tl-xl p-2">
+      <p class="text-white font-semibold">Configure a sua localização de partida dos serviços</p>
+    </div>
   </div>
 </template>
