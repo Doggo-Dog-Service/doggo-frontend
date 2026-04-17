1f5a0ab258cf73cfc85d0c9b0be1bd7b3097d250
Johann5731 <johannmborges27@gmail.com>
2026-04-17 14:54:53 -0300

    FEAT: Work in Progress #18

--- src/components/layouts/ProviderProfileTop.vue
+++ src/components/layouts/ProviderProfileTop.vue
@@ -30,30 +30,30 @@ const user = {
   <div class="bg-doggo-green h-80 relative overflow-hidden">
     <div class="absolute inset-0">
       <div :class="['blob', 'absolute top-0 left-0 w-100 h-100 rounded-full opacity-60 bg-white/5']"></div>
-      <div :class="['blob', 'absolute top-0 right-0 -translate-x-1/2 -translate-y-1/2 w-100 h-100 rounded-full opacity-60 bg-white/5']"></div>
+      <div :class="['blob', 'absolute top-0 left-0 w-100 h-100 rounded-full opacity-60 bg-white/5']"></div>
       <div :class="['blob', 'absolute bottom-0 left-0 w-100 h-100 rounded-full opacity-60 bg-white/5']"></div>
       <div :class="['blob', 'absolute bottom-0 right-0 w-100 h-100 rounded-full opacity-60 bg-white/5']"></div>
     </div>
   </div>
   <img
     v-if="user.profile_photo"
-    class="h-22 w-22 rounded-2xl z-1 border-white border-4 -translate-y-16 translate-x-8"
+    class="h-22 w-22 rounded-2xl z-1 border-white border-4 -translate-y-12 translate-x-6"
     :src="user.profile_photo"
     :alt="`${user.full_name.toLowerCase()}-photo`"
   />
   <div
     v-else
-    class="flex flex-col items-center justify-center h-22 w-22 rounded-2xl bg-linear-to-r from-doggo-green to-doggo-light-green z-1 border-white border-2 -translate-y-16 translate-x-8"
+    class="flex flex-col items-center justify-center h-22 w-22 rounded-2xl bg-linear-to-r from-doggo-green to-doggo-light-green z-1 border-white border-2 -translate-y-12 translate-x-8"
   >
     <p class="text-white text-xl">
       {{ user.full_name.charAt() }}
     </p>
   </div>
-  <p class="text-black -translate-y-22 ml-35 text-2xl font-bold">
+  <p class="text-black -translate-y-21 ml-30 text-2xl font-bold">
     {{ user.full_name }}
   </p>
   <div class="flex flex-nowrap w-1/3">
-    <p class="whitespace-nowrap ml-56 -translate-22 text-gray-400">
+    <p class="whitespace-nowrap ml-51 -translate-22 text-gray-400">
       <span class="mdi mdi-map-marker"></span>
       {{ user.location_home }}
     </p>
@@ -62,5 +62,16 @@ const user = {
     >
       Editar
     </button>
+  </div>
+  <div class="bg-background-light border border-doggo-gray rounded-2xl h-40 mx-2">
+    <div class="">
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
   </div>
 </template>
