5467c4b7a2f9fd7e63d1346683216cdd8963921b
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-22 22:47:25 -0300

    FEAT: RegisterView responsiveness #22

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -30,7 +30,7 @@ const changeProfileType = (type) => {
   profileType.value = type
 }
 
-onMounted( async() => {
+onMounted(async () => {
   await serviceStore.getServices()
 })
 
@@ -61,38 +61,42 @@ const handleRegister = async () => {
 </script>
 
 <template>
-  <div class="flex flex-col gap-6 w-full p-6">
-    <section class="flex flex-col">
-      <h1 class="text-doggo-black text-2xl font">Criar conta</h1>
-      <p class="text-zinc-400">Preencha os dados para começar</p>
-    </section>
-    <section class="flex flex-col gap-2">
-      <h2 class="font-semibold">Tipo de conta</h2>
-      <div class="flex gap-4">
-        <ChoseButton
-          text="Tutor"
-          sub-text="Dono do pet"
-          :selected="profileType === 'client'"
-          @select="changeProfileType('client')"
-        />
-        <ChoseButton
-          text="Profissional"
-          sub-text="Prestador de serviço"
-          :selected="profileType === 'provider'"
-          @select="changeProfileType('provider')"
-        />
+  <div class="flex flex-col gap-6 w-full p-6 md:flex-row md:gap-15">
+    <section class="w-full flex flex-col md:w-1/2 md:items-center md:gap-10">
+      <div class="w-full flex flex-col md:items-center">
+        <h1 class="text-doggo-black text-2xl font">Criar conta</h1>
+        <p class="text-zinc-400">Preencha os dados para começar</p>
       </div>
-      <div v-if="profileType === 'provider'" class="flex flex-col gap-2">
-        <h2 class="font-semibold">Tipo de serviço <span class="text-red-400">*</span></h2>
-        <ChoseButton v-for="(typeService, index) in serviceStore.typeServices" :key="index"
-          :text="typeService.name"
-          :sub-text="typeService.description"
-          :selected="typeService.id === typeServiceId"
-          @select="typeServiceId = typeService.id"
-        />
+      <div class="w-full flex flex-col gap-2">
+        <h2 class="font-semibold">Tipo de conta</h2>
+        <div class="flex gap-4">
+          <ChoseButton
+            text="Tutor"
+            sub-text="Dono do pet"
+            :selected="profileType === 'client'"
+            @select="changeProfileType('client')"
+          />
+          <ChoseButton
+            text="Profissional"
+            sub-text="Prestador de serviço"
+            :selected="profileType === 'provider'"
+            @select="changeProfileType('provider')"
+          />
+        </div>
+        <div v-if="profileType === 'provider'" class="w-full flex flex-col gap-2">
+          <h2 class="font-semibold">Tipo de serviço <span class="text-red-400">*</span></h2>
+          <ChoseButton
+            v-for="(typeService, index) in serviceStore.typeServices"
+            :key="index"
+            :text="typeService.name"
+            :sub-text="typeService.description"
+            :selected="typeService.id === typeServiceId"
+            @select="typeServiceId = typeService.id"
+          />
+        </div>
       </div>
     </section>
-    <form @submit.prevent="handleRegister" class="flex flex-col gap-4">
+    <form @submit.prevent="handleRegister" class="w-full flex flex-col gap-4 md:w-1/2">
       <AppInput
         icon="mdi mdi-account-outline"
         label="Nome completo"
@@ -142,10 +146,10 @@ const handleRegister = async () => {
         type="submit"
         :disabled="authStore.loading"
       />
+      <div class="flex justify-center gap-2">
+        <p>Já possui uma conta?</p>
+        <RouterLink class="font-bold text-doggo-green" to="/login">Entrar</RouterLink>
+      </div>
     </form>
-    <div class="flex justify-center gap-2">
-      <p>Já possui uma conta?</p>
-      <RouterLink class="font-bold text-doggo-green" to="/login">Entrar</RouterLink>
-    </div>
   </div>
 </template>
