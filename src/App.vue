6766aaa646600f956fecc00ca5353db8ee4e20ce
AndreRicardoSc <andrenarutto7@gmail.com>
2026-04-23 22:10:41 -0300

    FEAT: Increment transition page animations #22

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@ const route = useRoute()
 
 const isAppHeader = computed(() => route.path === '/')
 const isLoginView = computed(() => route.path === '/login')
-const isRegisterView = computed(() => route.path === '/register' || route.name === 'register-location-view')
+const isRegisterView = computed(
+  () => route.path === '/register' || route.name === 'register-location-view',
+)
 
 const isAuthPage = computed(() => isLoginView.value || isRegisterView.value)
 
@@ -24,8 +26,23 @@ usePresence()
       <AltHeader v-if="!isAppHeader && !isLoginView" />
     </header>
     <main class="flex-1 text-doggo-black">
-      <RouterView />
+      <RouterView v-slot="{ Component }">
+        <Transition
+          mode="out-in"
+          enter-active-class="transition duration-300 ease-out"
+          leave-active-class="transition duration-200 ease-in"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <component 
+            :is="Component" 
+            :key="$route.fullPath"
+          />
+        </Transition>
+      </RouterView>
     </main>
-    <MobileNavBar v-if="!isAuthPage " />
+    <MobileNavBar v-if="!isAuthPage" />
   </div>
 </template>
