01c9f9a930f5f06f9e744374cb034b4312bf2dff
AndreRicardoSc <andrenarutto7@gmail.com>
2026-05-11 13:40:02 -0300

    FEAT: Created SideBar

--- src/components/buttons/LogOutButton.vue
+++ src/components/buttons/LogOutButton.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <button class="flex text-red-400 items-center gap-4 px-4 py-2 rounded-lg transition-all duration-200 cursor-pointer hover:bg-red-400/20" @click="emits('logout')">
+  <button class="flex text-red-400 items-center w-full gap-4 px-4 py-2 rounded-lg transition-all duration-200 cursor-pointer hover:bg-red-400/20" @click="emits('logout')">
     <component v-if="props.icon" :is="props.icon" class="w-5 h-5 stroke-2"/>
     <p class="text-base font-semibold">{{ props.text }}</p>
   </button>
--- src/components/layouts/SideBar.vue
+++ src/components/layouts/SideBar.vue
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/stores/auth'
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import NavButton from '../buttons/NavButton.vue'
-import { ArrowLeftEndOnRectangleIcon, Cog6ToothIcon } from '@heroicons/vue/24/outline'
+import { ArrowLeftEndOnRectangleIcon, Cog6ToothIcon, BellIcon } from '@heroicons/vue/24/outline'
 import LogOutButton from '../buttons/LogOutButton.vue'
 const authStore = useAuthStore()
 const router = useRouter()
@@ -15,29 +15,69 @@ const isRouteActive = (path) => router.currentRoute.value.path === path
 
 onMounted(() => {
   routes.value = router.getRoutes().filter((route) => route.meta.isView)
+  console.log(authStore.user)
 })
 </script>
 
 <template>
-  <aside class="fixed top-0 left-0 w-72 h-screen flex flex-col bg-white">
-    <div>
-      <RouterLink class="w-full flex flex-col items-center py-6" to="/">
-        <img class="w-25" src="/logo.png" alt="logo" />
-      </RouterLink>
-      <nav class="flex flex-col gap-1 w-full px-6 py-4">
-        <NavButton
-          v-for="(route, index) in routes"
-          :key="index"
-          :to="route.path"
-          :icon="route.meta.icon"
-          :text="route.meta.title"
-          :active="isRouteActive(route.path)"
-        />
-      </nav>
-    </div>
-    <div class="mt-auto flex flex-col px-6 gap-1">
-      <NavButton :to="userConfigRouter" :icon="Cog6ToothIcon" text="Configurações" :active="isRouteActive(userConfigRouter)"/>
-      <LogOutButton :icon="ArrowLeftEndOnRectangleIcon" text="Sair" @logout="authStore.logout"/>
-    </div>
+  <aside class="bg-white h-screen fixed w-72 p-6">
+    <nav class="flex flex-col justify-between h-full">
+      <ul class="grid gap-1">
+        <li class="mb-3 flex flex-col">
+          <RouterLink to="/">
+            <img class="w-25" src="/logo.png" alt="logo" />
+          </RouterLink>
+        </li>
+        <li v-for="(route, index) in routes" :key="index">
+          <NavButton
+            :to="route.path"
+            :icon="route.meta.icon"
+            :text="route.meta.title"
+            :active="isRouteActive(route.path)"
+          />
+        </li>
+      </ul>
+
+      <ul class="grid gap-5">
+        <li class="grid gap-1 border-y border-doggo-gray py-5">
+          <NavButton
+            :to="userConfigRouter"
+            :icon="Cog6ToothIcon"
+            text="Configurações"
+            :active="isRouteActive(userConfigRouter)"
+          />
+          <LogOutButton
+            :icon="ArrowLeftEndOnRectangleIcon"
+            text="Sair"
+            @logout="authStore.logout"
+          />
+        </li>
+        <li class="grid grid-cols-4 items-center  gap-4 w-full">
+          <img
+            class="col-span-1 w-12 h-12 object-cover rounded-full"
+            v-if="authStore.user?.profile_picture?.file"
+            :src="authStore.user?.profile_picture?.file"
+            alt="profile_picture"
+          />
+          <div
+            v-else
+            class="w-12 h-12 bg-doggo-light-green grid items-center justify-center rounded-full text-white"
+          >
+            {{ authStore.user?.full_name[0] }}
+          </div>
+          <div class="col-span-2">
+            <h2 class="text-base truncate font-semibold">
+              {{ authStore.user?.full_name }}
+            </h2>
+            <p class="text-sm font-medium text-gray-400">
+              {{ authStore.user?.provider_profile ? authStore.user?.provider_profile?.service_type_detail?.name : 'Cliente' }}
+            </p>
+          </div>
+          <RouterLink to="/notifications" class="grid col-span-1 justify-center items-center p-2 w-fit h-fit rounded-xl transition-all duration-200 hover:bg-gray-400/20 ">
+            <BellIcon class="w-5 h-5 text-gray-400" />
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
   </aside>
 </template>
